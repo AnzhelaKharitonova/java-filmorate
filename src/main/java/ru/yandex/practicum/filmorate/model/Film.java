abee6a03c1060674eadc72f219d5219f3855fccd
AnzhelaKharitonova <haritonova.azazel88@yandex.ru>
2026-06-12 11:21:35 +0300

    Исправить валидацию releaseDate, раскомментировать код

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
